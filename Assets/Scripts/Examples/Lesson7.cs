887f06e64e65c882447a23e7d0f5132a5c86065a
Kuznetsova A <kuzanastyav@gmail.com>
2022-06-11 00:59:33 +0300

    Lesson7

---
+++ Assets/Scripts/Examples/Lesson7.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Lesson7
+{
+    internal static class Lesson7
+    {
+
+        //2. Реализовать метод расширения для поиска количество символов в строке.
+        public static int CountSymbolWithLINQ(this string str, char symbol)
+        {
+            //Реализация с linq
+            return str.Count(x => x == symbol);
+        }
+
+        public static int CountSymbol(this string str, char symbol)
+        {
+            var res = 0;
+            foreach (var s in str)
+            {
+                if(s == symbol)
+                    res++;
+            }
+            return res;
+        }
+
+        //3. Дана коллекция List<T>.Требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции:
+        //а.для целых чисел;
+        public static Dictionary<int, int> CountEachIntElement(List<int> list)
+        {
+            Dictionary<int, int> dict = new Dictionary<int,int>();
+            foreach (var item in list)
+            {
+                if(dict.ContainsKey(item))
+                    dict[item]++;
+                else
+                    dict[item] = 1;
+            }
+            return dict;
+        }
+
+        //b. * для обобщенной коллекции;
+        public static Dictionary<T, int> CountEachTElement<T>(List<T> list)
+        {
+            Dictionary<T, int> dict = new Dictionary<T, int>();
+            foreach (var item in list)
+            {
+                
+                if(dict.ContainsKey(item))
+                    dict[item]++;
+                else
+                    dict[item] = 1;
+            }
+            return dict;
+        }
+
+        // с. ** используя Linq.
+        public static Dictionary<T, int> CountEachTElementWithLinq<T>(List<T> list)
+        {
+            return list.GroupBy(g => g)
+                .Select(s => new KeyValuePair<T, int>(s.Key, s.Count()))
+                .ToDictionary(x => x.Key, x => x.Value);
+        }
+
+        //4. * В методичке дан фрагмент программы, необходимо:
+        //а.Свернуть обращение к OrderBy с использованием лямбда-выражения =>.
+        public static void Task4 ()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                {"four", 4 },
+                {"two", 2},
+                {"one", 1},
+                {"three", 3 }
+            };
+            var d = dict.OrderBy(x => x.Value);
+            foreach (var pair in d)
+            {
+                Debug.Log($"{pair.Key} - {pair.Value}");
+            }
+        }
+
+        //b. * Развернуть обращение к OrderBy с использованием делегата
+        static Func<KeyValuePair<string, int>, int> compare = (keyValuePair) => { return keyValuePair.Value; };
+        public static void Task4WithDelegate()
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>()
+            {
+                {"four", 4 },
+                {"two", 2},
+                {"one", 1},
+                {"three", 3 }
+            };
+
+            var d = dict.OrderBy(compare);
+            foreach (var pair in d)
+            {
+                Debug.Log($"{pair.Key} - {pair.Value}");
+            }
+        }
+    }
+}
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,6 +1,8 @@
+using Lesson7;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -46,6 +48,30 @@ namespace Maze
             _interactiveObjects.Add(_cameraController);
 
             InitiateBonuses();
+
+
+            var str = "Какая-то строка";
+            Debug.Log($"Кол-во символов \"а\" в строке \"{str}\": {str.CountSymbolWithLINQ('а')}");
+            Debug.Log($"Кол-во символов \"а\" в строке \"{str}\": {str.CountSymbol('а')}");
+
+            var intList = new List<int>{1, 2, 4, 6, 3, 3, 1, 2, 2, 9 };
+            Debug.Log($"Частотный словарь в интовом массиве: {string.Join(", ", intList)}");
+            var intRes = Lesson7.Lesson7.CountEachIntElement(intList).Select(x => $"[{x.Key}] = {x.Value}");
+            Debug.Log($"Словарь: {string.Join(", ", intRes)}");
+
+
+            var TList = new List<char> { '1', '2', '4', '6', '3', '3', '1', '2', '2', '9' };
+            Debug.Log($"Частотный словарь в Т массиве: {string.Join(", ", TList)}");
+            var TListRes = Lesson7.Lesson7.CountEachTElementWithLinq<char>(TList).Select(x => $"[{x.Key}] = {x.Value}");
+            Debug.Log($"Словарь: {string.Join(", ", TListRes)}");
+
+            var lTList = new List<char> { '1', '2', '4', '6', '3', '3', '1', '2', '2', '9' };
+            Debug.Log($"Частотный словарь в Т массиве: {string.Join(", ", lTList)}");
+            var lTListRes = Lesson7.Lesson7.CountEachTElementWithLinq<char>(lTList).Select(x => $"[{x.Key}] = {x.Value}");
+            Debug.Log($"Словарь: {string.Join(", ", lTListRes)}");
+
+            Lesson7.Lesson7.Task4();
+            Lesson7.Lesson7.Task4WithDelegate();
         }
 
         private void InitiateBonuses()
